7c1b408c66a1813e55ffb808799f9256e6e9974d
Md Sarfaraz Alam <sarfarazstark@gmail.com>
2024-08-06 01:51:01 +0530

    DSA - Insertion Sort using for loops

--- DSA.js
+++ DSA.js
@@ -199,11 +199,10 @@ console.log('----Insertion Sorting----');
 
 const insertionSort = function (arr) {
 	for (let i = 1; i < arr.length; i++) {
-		let j = i;
-
-		while (j >= 1 && arr[j - 1] > arr[j]) {
-			[arr[j - 1], arr[j]] = [arr[j], arr[j - 1]];
-			j -= 1;
+		for (let j = i; j >= 1; j--) {
+			if (arr[j - 1] > arr[j]) {
+				[arr[j - 1], arr[j]] = [arr[j], arr[j - 1]];
+			}
 		}
 	}
 };
